f20ee6a455fa2a0e891b6c666efd922286d4fff0
James Esteban <james.esteban08@gmail.com>
2025-07-23 09:17:52 +0800

    made tsx auth file

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+// import { useEffect, useState } from 'react';
+// import { Redirect, Stack } from 'expo-router';
+// import { getUserId } from '@utils/session';
+
+// export default function Index() {
+//   const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+//   useEffect(() => {
+//     const checkSession = async () => {
+//       const userId = await getUserId();
+//       setIsLoggedIn(!!userId);
+//     };
+
+//     checkSession();
+//   }, []);
+
+//   if (isLoggedIn === null) return null; // ⏳ wait for session check
+
+//   return (
+//     <>
+//       <Stack.Screen options={{ headerShown: false }} />
+//       <Redirect href={isLoggedIn ? '/(home)/dashboard' : '/login'} />
+//     </>
+//   );
+// }
